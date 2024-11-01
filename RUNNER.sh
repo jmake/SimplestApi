c404739f32c7240e5bb01c62c74de4f607930a7a
jmake <zvl_2006@hotmail.com>
2024-11-01 16:19:59 +0100

    Create RUNNER.sh

---
+++ RUNNER.sh
@@ -0,0 +1,20 @@
+git clone https://github.com/YunzheZJU/youtube-po-token-generator.git  
+
+npm install jsdom
+#node youtube-po-token-generator/examples/one-shot.js
+
+output=$(node.exe youtube-po-token-generator/examples/one-shot.js)
+
+# Print the raw output for debugging
+echo "Raw Output:"
+echo "$output"
+
+# Extract visitorData and poToken manually
+visitorData=$(echo "$output" | sed -n "s/.*visitorData: '\([^']*\)'.*/\1/p")
+poToken=$(echo "$output" | sed -n "s/.*poToken: '\([^']*\)'.*/\1/p")
+
+echo -e "${visitorData}\n${poToken}"
+
+# Run the Python script with the extracted values
+echo -e "${visitorData}\n${poToken}" | python.exe youtube_tools.py
+
