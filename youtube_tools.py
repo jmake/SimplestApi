b6283a634bebedd35cf85d0dce23f57146c97473
jmake <zvl_2006@hotmail.com>
2024-11-01 15:42:50 +0100

    Create youtube_tools.py

---
+++ youtube_tools.py
@@ -0,0 +1,32 @@
+import os 
+import time
+import subprocess
+
+from pytubefix import YouTube
+from pytubefix.cli import on_progress
+
+def list_directory(path="."):
+    try:
+        for entry in os.listdir(path):
+            print(entry)
+    except PermissionError:
+        print(f"Permission denied: {path}")
+
+def download_video_segment(video_id):
+  url = f"https://www.youtube.com/watch?v={video_id}"
+  #print( url )
+  print(f"download_video_segment:'{url}'")
+  
+  yt = YouTube(url, on_progress_callback = on_progress) #, use_po_token=True)
+  print(f"title:'{yt.title}'")
+
+  ys = yt.streams.get_highest_resolution()
+  ys.download(filename="temp_video.mp4")
+  
+  #list_directory( os.getcwd() )
+  
+
+if __name__ == "__main__" :
+    video_id="paxrBTnda78"
+    download_video_segment(video_id) 
+
